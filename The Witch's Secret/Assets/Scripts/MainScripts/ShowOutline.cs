b33d7b59305474424924da8741a1209b2fe4b8b8
MPaladin16 <mp52512@fer.hr>
2021-11-06 10:55:16 +0100

    Outlines done
    :)

---
+++ The Witch's Secret/Assets/Scripts/MainScripts/ShowOutline.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowOutline : MonoBehaviour
+{
+    public Sprite sprite;
+    public Sprite outlinedSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver() {
+        transform.GetComponent<SpriteRenderer>().sprite = outlinedSprite;
+
+    }
+
+    void OnMouseExit()
+    {
+        transform.GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+}
