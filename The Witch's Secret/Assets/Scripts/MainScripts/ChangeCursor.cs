ce382941455f05dc6ac3f7d8d59544c8e102cb00
d_arih <ja.jakovac@gmail.com>
2022-01-26 02:51:32 +0100

    Add custom cursor
    -

---
+++ The Witch's Secret/Assets/Scripts/MainScripts/ChangeCursor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Texture2D cursorArrow;
+    void Start()
+    {
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
